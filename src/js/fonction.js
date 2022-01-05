44f895f19a8efd21b4937ff16249d2f908926f00
Flo714 <florentin.demortiere@edu.univ-fcomte.fr>
2022-01-05 16:23:21 +0100

    Fin Carrousel + debut legende

--- src/js/fonction.js
+++ src/js/fonction.js
@@ -13,8 +13,8 @@ function carrousel9Images() {
 
     anim155.add({
         targets: images,
-        translateX: [{value: '500', duration: d},
-                     {value: '-500', duration: 0, delay: function(img,ind) {
+        translateX: [{value: '199', duration: d},
+                     {value: '-199', duration: 0, delay: function(img,ind) {
                          if (ind == 0) return (8*delta)+(7*d);
                          if (ind == 1) return (7*delta)+(6*d);
                          if (ind == 2) return (6*delta)+(5*d);
@@ -35,11 +35,33 @@ function carrousel9Images() {
             if (ind == 1) return (2*delta)+d;
             if (ind == 2) return (3*delta)+(2*d);
             if (ind == 3) return (4*delta)+(3*d);
-            if (ind == 2) return (5*delta)+(4*d);
-            if (ind == 2) return (6*delta)+(5*d);
-            if (ind == 2) return (7*delta)+(6*d);
-            if (ind == 2) return (8*delta)+(7*d);
+            if (ind == 4) return (5*delta)+(4*d);
+            if (ind == 5) return (6*delta)+(5*d);
+            if (ind == 6) return (7*delta)+(6*d);
+            if (ind == 7) return (8*delta)+(7*d);
             return (9*delta)+(8*d);
         }
     });
+}
+
+function monterLegende() {
+    document.querySelector('.CP_legende_roman_cp').style.display = 'block';
+    return anime({
+        targets: '.CP_legende_roman_cp',
+        translateY: '-5em',
+        autoplay: false,
+        easing: 'linear',
+        duration: 200
+    }).play();
+}
+
+function descendreLegende() {
+	document.querySelector('.CP_legende_roman_cp').style.display = 'block';
+    return anime({
+        targets: '.CP_legende_roman_cp',
+        translateY: '5em',
+        autoplay: false,
+        easing: 'linear',
+        duration: 200
+    }).play();
 }
